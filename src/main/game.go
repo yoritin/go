6f732d7c84e0fe84dd41c19eda0fb54facf1ba38
Makoto Katayori <47884591+yoritin@users.noreply.github.com>
2020-09-05 00:23:27 +0900

    add game.go

---
+++ src/main/game.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+func main() {
+	// 現在時刻を数値で取得する
+	t := time.Now().UnixNano()
+	// 乱数のたねを設定
+	rand.Seed(t)
+	// xは0-10の間の値になる
+	s := rand.Intn(6)
+	// println(s)
+	switch s {
+		case 6:
+			println("大吉")
+		case 5, 4:
+			println("中吉")
+		case 3, 2:
+			println("吉")
+		case 1:
+			println("凶")
+	}
+}
