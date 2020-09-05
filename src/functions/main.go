db7252b940dca8eeafd8048881eabe94fadffb0c
Makoto Katayori <47884591+yoritin@users.noreply.github.com>
2020-09-05 14:55:39 +0900

    add func

--- src/functions/main.go
+++ src/functions/main.go
@@ -4,10 +4,18 @@ import (
 	"fmt"
 )
 
-func add(x int, y int) int {
+func add(x, y int) int {
 	return x + y
+}
+
+func swap(x, y int) (x2, y2 int) {
+	y2, x2 = y, x
+	return
 }
 
 func main() {
 	fmt.Println(add(10, 20))
+	fmt.Println(swap(10, 20))
+	x2, y2 := swap(10, 20)
+	fmt.Println(x2, y2)
 }
